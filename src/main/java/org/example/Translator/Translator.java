02f00ae305ca0d89796e230610918188b408cdd8
tazik <gor.prast@bk.ru>
2024-11-10 22:09:24 +0500

    add method getTranslate what works with list of string

--- src/main/java/org/example/Translator/Translator.java
+++ src/main/java/org/example/Translator/Translator.java
@@ -4,6 +4,7 @@ import org.example.Translator.TranslationPOJOs.TranslationResponse;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Translator implements ITranslator {
@@ -14,6 +15,17 @@ public class Translator implements ITranslator {
         TranslationResponse response = client.getTranslate(List.of(text));
         if (response.getTranslations() != null && !response.getTranslations().isEmpty()) {
             return response.getTranslations().get(0).getText();
+        }
+        else throw new IOException();
+    }
+
+    public List<String> translate(List<String> text) throws IOException {
+        TranslationResponse response = client.getTranslate(text);
+        if (response.getTranslations() != null && !response.getTranslations().isEmpty()) {
+            List<String> answer = new ArrayList<>();
+            for (var translation : response.getTranslations())
+                answer.add(translation.getText());
+            return answer;
         }
         else throw new IOException();
     }
