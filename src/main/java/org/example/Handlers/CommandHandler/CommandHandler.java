179f0a116c1f3cebd626725563a979145a8e72ee
tazik <gor.prast@bk.ru>
2024-11-10 23:13:02 +0500

    feat: add handle method realization in CommandHandler

--- src/main/java/org/example/Handlers/CommandHandler/CommandHandler.java
+++ src/main/java/org/example/Handlers/CommandHandler/CommandHandler.java
@@ -2,7 +2,9 @@ package org.example.Handlers.CommandHandler;
 
 import org.example.Handlers.CommandHandler.Commands.ICommand;
 import org.example.Handlers.IHandler;
+import org.example.Utils.Consts;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
@@ -17,6 +19,16 @@ public class CommandHandler implements IHandler {
 
     @Override
     public List<PartialBotApiMethod> handle(Update update) {
-        return List.of();
+        Long chatId = update.getMessage().getChatId();
+        String message = update.getMessage().getText();
+
+        try {
+            CommandType commandType = CommandType.fromDescription(message);
+            ICommand command = commands.get(commandType);
+            return command.apply(chatId);
+        }
+        catch (IllegalArgumentException e) {
+            return List.of(new SendMessage(String.valueOf(chatId), Consts.UNKNOWN_COMMAND));
+        }
     }
 }
