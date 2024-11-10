00dc20168bb93ff4d329efffae8c3cc69aafee48
tazik <gor.prast@bk.ru>
2024-11-10 19:15:58 +0500

    feat: add Bot class

--- src/main/java/org/example/Bot/ThousandsBot.java
+++ src/main/java/org/example/Bot/ThousandsBot.java
@@ -1,4 +1,20 @@
 package org.example.Bot;
 
-public class ThousandsBot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class ThousandsBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return System.getenv("BOT_NAME");
+    }
+    @Override
+    public String getBotToken() {
+        return System.getenv("BOT_TOKEN");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        
+    }
 }
