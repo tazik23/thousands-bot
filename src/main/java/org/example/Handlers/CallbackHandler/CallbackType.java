c016590ab46dccf03857011c85f93c257891bb9b
tazik <gor.prast@bk.ru>
2024-11-10 23:11:56 +0500

    add fromDescription method in CallbackType

--- src/main/java/org/example/Handlers/CallbackHandler/CallbackType.java
+++ src/main/java/org/example/Handlers/CallbackHandler/CallbackType.java
@@ -6,12 +6,22 @@ public enum CallbackType {
     DICTIONARY("dictionary");
 
     private final String description;
+
     private CallbackType(String description)
     {
         this.description = description;
     }
+
     public String getDescription() {
         return description;
     }
 
+    public static CallbackType fromDescription(String description) {
+        for (CallbackType value : CallbackType.values()) {
+            if (value.getDescription().equalsIgnoreCase(description)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant with description " + description);
+    }
 }
