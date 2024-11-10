73faab8e0387bc458fe597c134b4bd79f56cc4c9
tazik <gor.prast@bk.ru>
2024-11-10 20:26:25 +0500

    feat: add TranslationRequest POJO

--- src/main/java/org/example/Translator/TranslationPOJOs/TranslationRequest.java
+++ src/main/java/org/example/Translator/TranslationPOJOs/TranslationRequest.java
@@ -1,4 +1,31 @@
 package org.example.Translator.TranslationPOJOs;
 
+
+import java.util.List;
+
 public class TranslationRequest {
+
+    private final String folderId;
+    private final List<String> texts;
+    private final String targetLanguageCode;
+
+    public TranslationRequest(String folderId,
+                              List<String> texts,
+                              String targetLanguageCode) {
+        this.folderId = folderId;
+        this.texts = texts;
+        this.targetLanguageCode = targetLanguageCode;
+    }
+
+    public String getFolderId() {
+        return folderId;
+    }
+
+    public List<String> getTexts() {
+        return texts;
+    }
+
+    public String getTargetLanguageCode() {
+        return targetLanguageCode;
+    }
 }
