b570307480abf79bbaf445a6c96ac7fa9959e3f6
tazik <gor.prast@bk.ru>
2024-11-10 20:26:46 +0500

    feat: add TranslationResponse POJO

--- src/main/java/org/example/Translator/TranslationPOJOs/TranslationResponse.java
+++ src/main/java/org/example/Translator/TranslationPOJOs/TranslationResponse.java
@@ -1,4 +1,16 @@
 package org.example.Translator.TranslationPOJOs;
 
+import java.util.ArrayList;
+
 public class TranslationResponse {
+
+    private ArrayList<Translation> translations;
+
+    public ArrayList<Translation> getTranslations() {
+        return translations;
+    }
+
+    public void setTranslations(ArrayList<Translation> translations) {
+        this.translations = translations;
+    }
 }
