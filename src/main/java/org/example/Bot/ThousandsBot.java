80921b79e549d57c49d8bda5b9d1b7ca5fa53ec9
tazik <gor.prast@bk.ru>
2024-11-10 23:34:07 +0500

    feat: add required classes in bot

--- src/main/java/org/example/Bot/ThousandsBot.java
+++ src/main/java/org/example/Bot/ThousandsBot.java
@@ -1,9 +1,18 @@
 package org.example.Bot;
 
+import org.example.Handlers.IHandler;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class ThousandsBot extends TelegramLongPollingBot {
+    private final IHandler commandHandler;
+    private final IHandler callbackHandler;
+
+    public ThousandsBot(IHandler commandHandler, IHandler callbackHandler){
+        this.commandHandler = commandHandler;
+        this.callbackHandler = callbackHandler;
+    }
+
     @Override
     public String getBotUsername() {
         return System.getenv("BOT_NAME");
