955557661e9eba6e0d2857f05ebbeb44fa14f9ab
tazik <gor.prast@bk.ru>
2024-11-10 23:01:03 +0500

    feat: add required dependencies in ThousandsCommand class

--- src/main/java/org/example/Handlers/CommandHandler/Commands/ThousandsCommand.java
+++ src/main/java/org/example/Handlers/CommandHandler/Commands/ThousandsCommand.java
@@ -1,10 +1,20 @@
 package org.example.Handlers.CommandHandler.Commands;
 
+import org.example.ArticleServices.IArticleFinder;
+import org.example.Repositories.ISessionRepository;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 
 import java.util.List;
 
 public class ThousandsCommand implements ICommand{
+    private final IArticleFinder articleFinder;
+    private final ISessionRepository sessionRepository;
+
+    public ThousandsCommand(IArticleFinder articleFinder, ISessionRepository sessionRepository){
+        this.articleFinder = articleFinder;
+        this.sessionRepository = sessionRepository;
+    }
+    
     @Override
     public List<PartialBotApiMethod> apply(long id) {
         return List.of();
