499f805140322595d99b1ae74d3295032b7bd739
tazik <gor.prast@bk.ru>
2024-11-10 22:12:20 +0500

    feat: add realization for Translator translateFile method

--- src/main/java/org/example/Translator/Translator.java
+++ src/main/java/org/example/Translator/Translator.java
@@ -1,8 +1,12 @@
 package org.example.Translator;
 
 import org.example.Translator.TranslationPOJOs.TranslationResponse;
+import org.example.Utils.io.DocxReader;
+import org.example.Utils.io.DocxWriter;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +36,28 @@ public class Translator implements ITranslator {
 
     @Override
     public File translateFile(File file) throws IOException {
-        return null;
+        File translatedFile = new File("Translated" + file.getName());
+        DocxReader reader = new DocxReader(new FileInputStream(file));
+        DocxWriter writer = new DocxWriter(new FileOutputStream(translatedFile));
+
+        try {
+            String paragraph;
+            List<String> paragraphs = new ArrayList<>();
+            List<String> translatedParagraphs;
+            while((paragraph = reader.readParagraph()) != null) {
+                if(paragraph.isEmpty())
+                    continue;
+                paragraphs.add(paragraph);
+            }
+
+            translatedParagraphs = translate(paragraphs);
+            for(var translatedParagraph : translatedParagraphs)
+                writer.writeParagraph(translatedParagraph);
+            writer.save();
+            return translatedFile;
+        }
+        catch (IOException e) {
+            return null;
+        }
     }
 }
