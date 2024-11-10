19c791a0dee02d21cb12212d3efad64b7ff9017a
tazik <gor.prast@bk.ru>
2024-11-10 23:09:51 +0500

    feat: add map of proccesors in CommandHandler

--- src/main/java/org/example/Handlers/CommandHandler/CommandHandler.java
+++ src/main/java/org/example/Handlers/CommandHandler/CommandHandler.java
@@ -1,12 +1,20 @@
 package org.example.Handlers.CommandHandler;
 
+import org.example.Handlers.CommandHandler.Commands.ICommand;
 import org.example.Handlers.IHandler;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
+import java.util.Map;
 
 public class CommandHandler implements IHandler {
+    private final Map<CommandType, ICommand> commands;
+
+    public CommandHandler(Map<CommandType, ICommand> commands){
+        this.commands = commands;
+    }
+
     @Override
     public List<PartialBotApiMethod> handle(Update update) {
         return List.of();
