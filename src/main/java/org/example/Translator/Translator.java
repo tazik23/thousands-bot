dda8c66d95398b3203d75141a062b9608d31cd0b
tazik <gor.prast@bk.ru>
2024-11-10 22:06:22 +0500

    feat: add realization for Translator getTranslate method

--- src/main/java/org/example/Translator/Translator.java
+++ src/main/java/org/example/Translator/Translator.java
@@ -1,12 +1,21 @@
 package org.example.Translator;
 
+import org.example.Translator.TranslationPOJOs.TranslationResponse;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
-public class Translator implements ITranslator{
+public class Translator implements ITranslator {
+    private final YandexTranslateClient client = new YandexTranslateClient();
+
     @Override
     public String translate(String text) throws IOException {
-        return "";
+        TranslationResponse response = client.getTranslate(List.of(text));
+        if (response.getTranslations() != null && !response.getTranslations().isEmpty()) {
+            return response.getTranslations().get(0).getText();
+        }
+        else throw new IOException();
     }
 
     @Override
