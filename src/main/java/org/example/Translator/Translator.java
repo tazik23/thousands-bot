e66a2d77632b4a71c9c0f21b00b305b972255ea4
tazik <gor.prast@bk.ru>
2024-11-10 22:04:58 +0500

    feat: add ITranslator impl

---
+++ src/main/java/org/example/Translator/Translator.java
@@ -0,0 +1,16 @@
+package org.example.Translator;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Translator implements ITranslator{
+    @Override
+    public String translate(String text) throws IOException {
+        return "";
+    }
+
+    @Override
+    public File translateFile(File file) throws IOException {
+        return null;
+    }
+}
