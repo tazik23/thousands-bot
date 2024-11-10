cece7b960d7abe4e8d819027e1c3436436f9eb6e
tazik <gor.prast@bk.ru>
2024-11-10 19:42:13 +0500

    feat: add ArticleParser interface

--- src/main/java/org/example/ArticleServices/IArticleParser.java
+++ src/main/java/org/example/ArticleServices/IArticleParser.java
@@ -1,4 +1,10 @@
 package org.example.ArticleServices;
 
+import org.example.Models.Article;
+
+import java.io.File;
+import java.io.IOException;
+
 public interface IArticleParser {
+    File parse(Article article) throws IOException;
 }
