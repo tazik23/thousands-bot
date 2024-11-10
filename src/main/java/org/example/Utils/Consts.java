68a2b2c4d7c9d9fcd3049225d4e991cd0d7f060c
tazik <gor.prast@bk.ru>
2024-11-10 22:54:22 +0500

    feat: add string consts for answers

---
+++ src/main/java/org/example/Utils/Consts.java
@@ -0,0 +1,15 @@
+package org.example.Utils;
+
+public class Consts {
+    public static final String START_MESSAGE = "привет, я бот для подготовки к \"Тысячам\", могу помочь с выбором статьи, ее переводом и составлением словарика. Для начала работы введите /thousands";
+    public static final String UNKNOWN_COMMAND = "извините, я пока не знаю такой команды(";
+    public static final String ERROR = "где-то ошибочка вышла...";
+    public static final String CHOOSE_ARTICLE = "пожалуйста, выберите статью:";
+    public static final String CHOOSE_TRANSLATION = "нужен ли перевод?";
+    public static final String CHOOSE_DICTIONARY = "нужен ли словарь?";
+    public static final String ANSWER = "все готово!";
+    public static final String ARTICLE_CHOSEN = "чего тыкаешь? уже выбрал, хочешь отменить - /cancel введи";
+    public static final String TRANSLATION_EXISTS = "ну что ты тыкаешь? перевод уже есть";
+    public static final String DICTIONARY_EXISTS = ".... уже есть";
+
+}
