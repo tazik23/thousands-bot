28bc95c0b8dcdf41f6e1ad4040e59312c2aa0128
tazik <gor.prast@bk.ru>
2024-11-10 20:36:56 +0500

    add getTranslate method in Client to connect with YandexTranslate

--- src/main/java/org/example/Translator/YandexTranslateClient.java
+++ src/main/java/org/example/Translator/YandexTranslateClient.java
@@ -1,6 +1,12 @@
 package org.example.Translator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.*;
+import org.example.Translator.TranslationPOJOs.TranslationRequest;
+import org.example.Translator.TranslationPOJOs.TranslationResponse;
+
+import java.io.IOException;
+import java.util.List;
 
 public class YandexTranslateClient {
     private final String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
@@ -10,5 +16,32 @@ public class YandexTranslateClient {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    public TranslationResponse getTranslate(List<String> texts) throws IOException {
+        OkHttpClient client = new OkHttpClient();
 
+        String json = objectMapper.writeValueAsString(new TranslationRequest(folderId, texts, targetLanguage));
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+        RequestBody body = RequestBody.create(json, JSON);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Content-Type", "application/json")
+                .header("Authorization", ("Api-Key " + ApiKey))
+                .post(body)
+                .build();
+
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                String responseBody = response.body().string();
+                return objectMapper.readValue(responseBody, TranslationResponse.class);
+            }
+        }
+        catch (IOException e) {
+            //todo: add logging
+        }
+
+        return null;
+    }
 }
