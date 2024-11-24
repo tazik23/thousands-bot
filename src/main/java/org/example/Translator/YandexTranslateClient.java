d1a86d5ed2449b927ee6c6c69de2e4d62515f9d8
tazik <gor.prast@bk.ru>
2024-11-24 17:49:41 +0500

    fix: add several supportive methods to improve readability

--- src/main/java/org/example/Translator/YandexTranslateClient.java
+++ src/main/java/org/example/Translator/YandexTranslateClient.java
@@ -13,15 +13,12 @@ public class YandexTranslateClient {
     private final String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
     private final String ApiKey = System.getenv("API_KEY");
     private final String folderId = System.getenv("FOLDER_ID");
-    private final String targetLanguage = "ru";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final OkHttpClient client = new OkHttpClient();
 
-    public TranslationResponse getTranslate(List<String> texts) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        RequestBody body = formRequestBody(texts);
-
+    public TranslationResponse getTranslate(List<String> texts, String targetLanguage) throws IOException {
+        RequestBody body = formRequestBody(texts, targetLanguage);
         Request request = new Request.Builder()
                 .url(url)
                 .header("Content-Type", "application/json")
@@ -29,22 +26,29 @@ public class YandexTranslateClient {
                 .post(body)
                 .build();
 
-
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful() && response.body() != null) {
-                String responseBody = response.body().string();
-                return objectMapper.readValue(responseBody, TranslationResponse.class);
-            }
-        }
-        catch (IOException e) {
-            //todo: add logging
-        }
-        return null;
+        return sendRequest(request);
     }
 
-    private RequestBody formRequestBody(List<String> texts) throws JsonProcessingException {
-        String json = objectMapper.writeValueAsString(new TranslationRequest(folderId, texts, targetLanguage));
+    private TranslationResponse sendRequest(Request request) throws IOException {
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Request error: " + response.code());
+            }
+            if (response.body() == null){
+                throw new IOException("Response body is null");
+            }
+
+            return objectMapper
+                    .readValue(response.body().string(), TranslationResponse.class);
+        }
+    }
+
+
+    private RequestBody formRequestBody(List<String> texts, String targetLanguage) throws JsonProcessingException {
+        String json = objectMapper
+                .writeValueAsString(new TranslationRequest(folderId, texts, targetLanguage));
         MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
         return RequestBody.create(json, JSON);
     }
 }
