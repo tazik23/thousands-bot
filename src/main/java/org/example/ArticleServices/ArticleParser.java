d49025e0f5c54575cbf0660ff8d7d902c4de822d
Rasul <rasulgasanbekov123@hmail.com>
2024-12-01 17:50:52 +0500

    chore: add files from feature/article-parser branch to current branch

---
+++ src/main/java/org/example/ArticleServices/ArticleParser.java
@@ -0,0 +1,56 @@
+package org.example.ArticleServices;
+
+import org.example.Models.Article;
+import org.example.Utils.io.DocxWriter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleParser implements IArticleParser {
+
+    @Override
+    public File parse(Article article) throws IOException {
+        String sanitizedTitle = article.getTitle()
+                .replaceAll(" ", "_")
+                .replaceAll("[\\\\/:*?\"<>|]", "");
+        File file = new File(sanitizedTitle + ".docx");
+        DocxWriter writer = new DocxWriter(new FileOutputStream(file));
+        List<String> articleTexts = getText(article);
+
+        try {
+            writer.writeParagraph("Title: " + article.getTitle() + "\n");
+            writer.writeParagraph("Link: " + article.getLink() + "\n");
+            writer.writeParagraph("Content:\n");
+            for (String paragraph : articleTexts) {
+                writer.writeParagraph(paragraph + "\n");
+            }
+            writer.save();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return file;
+    }
+
+    private List<String> getText(Article article) {
+        List<String> articleTexts = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(article.getLink()).get();
+            Elements paragraphs = doc.select(".entry-content p");
+            for (Element paragraph : paragraphs) {
+                articleTexts.add(paragraph.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return articleTexts;
+    }
+
+}
