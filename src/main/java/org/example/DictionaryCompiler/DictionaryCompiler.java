68576e5e0d290209799c1536606ca429e93cee28
tazik <gor.prast@bk.ru>
2024-12-01 17:59:07 +0500

    feat: add method to get complex words

--- src/main/java/org/example/DictionaryCompiler/DictionaryCompiler.java
+++ src/main/java/org/example/DictionaryCompiler/DictionaryCompiler.java
@@ -1,9 +1,17 @@
 package org.example.DictionaryCompiler;
 
+import org.example.DictionaryCompiler.Queue.BoundedPriorityBlockingQueue;
+import org.example.DictionaryCompiler.Queue.PrioritizedWord;
 import org.example.DictionaryCompiler.WordComplexityAnalyzers.WordFrequencyAnalyzer;
 import org.example.DictionaryCompiler.WordComplexityAnalyzers.WordLengthAnalyzer;
+import org.example.Utils.io.DocxReader;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class DictionaryCompiler implements IDictionaryCompiler{
     private final TextTokenizer tokenizer;
@@ -18,6 +26,34 @@ public class DictionaryCompiler implements IDictionaryCompiler{
 
     @Override
     public File compileDictionary(File file) {
+        return null;
+    }
+
+    private BoundedPriorityBlockingQueue getComplexWords(File file) {
+        BoundedPriorityBlockingQueue queue = new BoundedPriorityBlockingQueue(150);
+        Set<String> tokens = Collections.synchronizedSet(new HashSet<String>());
+
+        try(DocxReader reader = new DocxReader(file)) {
+            List<String> paragraphs = reader.read();
+
+            paragraphs.stream()
+                    .parallel()
+                    .forEach(p -> tokens.addAll(tokenizer.tokenize(p)));
+
+            tokens.stream()
+                    .parallel()
+                    .filter(t -> t != null && t.length() > 2)
+                    .forEach(t -> {
+                        double value = frequencyAnalyzer.analyze(t) + lengthAnalyzer.analyze(t);
+                        queue.add(new PrioritizedWord(t, value));
+                    });
+
+            return queue;
+
+        }
+        catch (Exception e) {
+
+        }
         return null;
     }
 }
